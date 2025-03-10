b905e510b3271db83bfd482cfce94e301f061e42
IrinaPerezhegina <perezeginaiv@mail.ru>
2025-03-10 17:20:56 +0500

    added CheckboxFiltersGroup

---
+++ components/shared/checkbox-filters-group.tsx
@@ -0,0 +1,69 @@
+"use client";
+import * as React from "react";
+import { Button, Input } from "../ui";
+import { FilterCheckbox, FilterCheckboxProps } from "./filter-checkbox";
+
+type Item = FilterCheckboxProps;
+
+interface Props {
+  title: string;
+  items: Item[];
+  defaultItems: Item[];
+  limit?: number;
+  searchInputPlaceholder?: string;
+  onChange?: (values: string[]) => void;
+  defaultValue?: string[];
+  classname?: string;
+}
+
+export const CheckboxFiltersGroup: React.FC<Props> = ({
+  title,
+  items,
+  defaultItems,
+  limit = 3,
+  searchInputPlaceholder = "Поиск...",
+  onChange,
+  defaultValue,
+  classname,
+}) => {
+  const [showAll, setShowAll] = React.useState<boolean>(false);
+  const list = showAll ? items : defaultItems?.slice(0, limit);
+  console.log(items.length);
+
+  return (
+    <div className={classname}>
+      <p className="font-bold mb-3">{title}</p>
+      {showAll && (
+        <div className="mb-5">
+          <Input
+            placeholder={searchInputPlaceholder}
+            className="bg-gray-50 border-none"
+          />
+        </div>
+      )}
+      <div className="flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar">
+        {list.map((item, index) => (
+          <FilterCheckbox
+            onCheckedChange={(itms) => console.log(itms)}
+            checked={false}
+            key={String(item.value)}
+            value={item.value}
+            text={item.text}
+            endAdornment={item.endAdornment}
+          />
+        ))}
+      </div>
+      {items.length > limit && (
+        <div className={showAll ? "border-t border-t-neutral-100 mt-4" : ""}>
+          <Button
+            onClick={() => setShowAll(!showAll)}
+            className="text-primary mt-3"
+          >
+            {showAll ? "Скрыть" : "+ Показать все"}
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+};
+// 1:48:15
--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Input } from "../ui";
+import { CheckboxFiltersGroup } from "./checkbox-filters-group";
 import { FilterCheckbox } from "./filter-checkbox";
 import { RangeSlider } from "./range-slider";
 import { Title } from "./title";
@@ -7,15 +8,57 @@ import { Title } from "./title";
 interface Props {
   className?: string;
 }
-
+const defaultItems = [
+  {
+    text: "Сырный соус",
+    value: "1",
+  },
+  {
+    text: "Моццарелла",
+    value: "2",
+  },
+  {
+    text: "Чеснок",
+    value: "3",
+  },
+];
+const items = [
+  {
+    text: "Сырный соус",
+    value: "1",
+  },
+  {
+    text: "Моццарелла",
+    value: "2",
+  },
+  {
+    text: "Чеснок",
+    value: "3",
+  },
+  {
+    text: "Соленые огурчики",
+    value: "4",
+  },
+  {
+    text: "Красный лук",
+    value: "5",
+  },
+  {
+    text: "Томаты",
+    value: "6",
+  },
+];
 export const Filters: React.FC<Props> = ({ className }) => {
   return (
     <div className={className}>
       <Title text="Фильтрация" size="sm" className="mb-5 font-bold" />
+
+      {/* Верхние чекбоксы */}
       <div className="flex flex-col gap-4">
         <FilterCheckbox text="Можно собирать" value="1" />
         <FilterCheckbox text="Новинки" value="12" />
       </div>
+      {/* Фильтр цен*/}
       <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
         <p className="font-bold mb-3"> Цена от до:</p>
         <div className="flex gap-3 mb-5">
@@ -30,7 +73,13 @@ export const Filters: React.FC<Props> = ({ className }) => {
         </div>
         <RangeSlider step={10} min={0} max={1000} value={[0, 1000]} />
       </div>
+      <CheckboxFiltersGroup
+        title="Ингредиенты"
+        items={items}
+        limit={6}
+        defaultItems={defaultItems}
+        classname="mt-5"
+      />
     </div>
   );
 };
-// 1:18:31
