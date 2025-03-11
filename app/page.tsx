f875b6e9bd49b9b41f692f4970824fe9bbcd7ed3
IrinaPerezhegina <perezeginaiv@mail.ru>
2025-03-11 16:45:56 +0500

    added prisma, api, store

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import {
   Container,
   Filters,
-  ProductCard,
+  ProductsGroupList,
   Title,
   TopBar,
 } from "@/components/shared";
@@ -15,7 +15,7 @@ export default function Home() {
 
       <TopBar />
       <Container className="pb-14 mt-10">
-        <div className="flex gap-[60px]">
+        <div className="flex gap-[70px]">
           {/*Фильтрация  */}
           <div className="w-[250px]">
             <Filters />
@@ -24,7 +24,86 @@ export default function Home() {
           {/*Список товаров   */}
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              <ProductCard id={34} imageUrl="ccc" name="ccc" price={200} />
+              <ProductsGroupList
+                key={1}
+                listClassName=""
+                categoryId={1}
+                title="Пиццы"
+                items={[
+                  {
+                    id: 1,
+                    name: "Чизбургер-пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11ef9a30c3246adebecb726548cbede9.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 2,
+                    name: "Чизбургер-пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11ef9a30c3246adebecb726548cbede9.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 3,
+                    name: "Чизбургер-пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11ef9a30c3246adebecb726548cbede9.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 4,
+                    name: "Чизбургер-пицца",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11ef9a30c3246adebecb726548cbede9.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+              />
+              <ProductsGroupList
+                key={2}
+                listClassName=""
+                categoryId={2}
+                title="Напитки"
+                items={[
+                  {
+                    id: 6,
+                    name: "Напиток",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11ee7d61ab1f976991362a5a42a2613e.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 7,
+                    name: "Напиток",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11ee7d61ab1f976991362a5a42a2613e.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 8,
+                    name: "Напиток",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11ee7d61ab1f976991362a5a42a2613e.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 9,
+                    name: "Напиток",
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:584x584/11ee7d61ab1f976991362a5a42a2613e.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+              />
             </div>
           </div>
         </div>
@@ -32,4 +111,3 @@ export default function Home() {
     </>
   );
 }
-// 2:00:12
--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -1,8 +1,10 @@
+"use client";
 import { cn } from "@/lib/utils";
+import { useCategoryStore } from "@/store/category";
 import * as React from "react";
+import { useIntersection } from "react-use";
 import { ProductCard } from "./product-card";
 import { Title } from "./title";
-
 interface Props {
   title: string;
   items: any[];
@@ -18,8 +20,19 @@ export const ProductsGroupList: React.FC<Props> = ({
   categoryId,
   className,
 }) => {
+  const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
+  const intersectionRef = React.useRef(null);
+  const intersection = useIntersection(intersectionRef, {
+    threshold: 0.4,
+  });
+
+  React.useEffect(() => {
+    if (intersection?.isIntersecting) {
+      setActiveCategoryId(categoryId);
+    }
+  }, [intersection?.isIntersecting]);
   return (
-    <div className={className}>
+    <div className={className} id={title} ref={intersectionRef}>
       <Title text={title} size="lg" className="font-extrabold mb-5" />
       <div className={cn("grid grid-cols-3 gap-[50px]", listClassName)}>
         {items.map((product, i) => (
