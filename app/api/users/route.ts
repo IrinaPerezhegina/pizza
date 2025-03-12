b45eddede5c0a91b3a702e3f450189713b9e859c
IrinaPerezhegina <perezeginaiv@mail.ru>
2025-03-12 13:21:26 +0500

    added prisma, api, store

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,12 +1,14 @@
 import { prisma } from "@/prisma/prisma-client";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   const users = await prisma.user.findMany();
   return NextResponse.json(users);
 }
-export async function POST() {
-  const user = await prisma.user.create;
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const user = await prisma.user.create({
+    data,
+  });
+  return NextResponse.json(user);
 }
-
-// 3:01:25
