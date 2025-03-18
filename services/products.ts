9f5936283adba41e85e82d3d3d56d2d46a783ea7
IrinaPerezhegina <perezeginaiv@mail.ru>
2025-03-18 09:32:24 +0500

    added ParamsUrl

--- services/products.ts
+++ services/products.ts
@@ -5,6 +5,7 @@ import { axiosInstance } from "./instance";
 export const search = async (query: string): Promise<Product[]> => {
   const { data } = await axiosInstance.get<Product[]>(
     ApiRoutes.SEARCH_PRODUCTS,
+
     {
       params: {
         query,
