fcfc39496de2e2c0f554c95092badf2c7874ee34
IrinaPerezhegina <perezeginaiv@mail.ru>
2025-04-17 09:59:16 +0500

    added HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,19 @@ import {
   Title,
   TopBar,
 } from "@/components/shared";
+import { prisma } from "@/prisma/prisma-client";
 
-export default function Home() {
+export default async function Home() {
+  const categories = await prisma.category.findMany({
+    include: {
+      products: {
+        include: {
+          ingredients: true,
+          vatiants: true,
+        },
+      },
+    },
+  });
   return (
     <>
       <Container className="mt-10">
@@ -24,86 +35,17 @@ export default function Home() {
           {/*Список товаров   */}
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              <ProductsGroupList
-                key={1}
-                listClassName=""
-                categoryId={1}
-                title="Пиццы"
-                items={[
-                  {
-                    id: 1,
-                    name: "Чизбургер-пицца",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:584x584/11ef9a30c3246adebecb726548cbede9.avif",
-                    price: 550,
-                    items: [{ price: 550 }],
-                  },
-                  {
-                    id: 2,
-                    name: "Чизбургер-пицца",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:584x584/11ef9a30c3246adebecb726548cbede9.avif",
-                    price: 550,
-                    items: [{ price: 550 }],
-                  },
-                  {
-                    id: 3,
-                    name: "Чизбургер-пицца",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:584x584/11ef9a30c3246adebecb726548cbede9.avif",
-                    price: 550,
-                    items: [{ price: 550 }],
-                  },
-                  {
-                    id: 4,
-                    name: "Чизбургер-пицца",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:584x584/11ef9a30c3246adebecb726548cbede9.avif",
-                    price: 550,
-                    items: [{ price: 550 }],
-                  },
-                ]}
-              />
-              <ProductsGroupList
-                key={2}
-                listClassName=""
-                categoryId={2}
-                title="Напитки"
-                items={[
-                  {
-                    id: 6,
-                    name: "Напиток",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:584x584/11ee7d61ab1f976991362a5a42a2613e.avif",
-                    price: 550,
-                    items: [{ price: 550 }],
-                  },
-                  {
-                    id: 7,
-                    name: "Напиток",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:584x584/11ee7d61ab1f976991362a5a42a2613e.avif",
-                    price: 550,
-                    items: [{ price: 550 }],
-                  },
-                  {
-                    id: 8,
-                    name: "Напиток",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:584x584/11ee7d61ab1f976991362a5a42a2613e.avif",
-                    price: 550,
-                    items: [{ price: 550 }],
-                  },
-                  {
-                    id: 9,
-                    name: "Напиток",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:584x584/11ee7d61ab1f976991362a5a42a2613e.avif",
-                    price: 550,
-                    items: [{ price: 550 }],
-                  },
-                ]}
-              />
+              {categories.map(
+                (category) =>
+                  category.products.length > 0 && (
+                    <ProductsGroupList
+                      key={category.id}
+                      title={category.name}
+                      items={category.products}
+                      categoryId={category.id}
+                    />
+                  )
+              )}
             </div>
           </div>
         </div>
