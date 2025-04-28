49dfe53ad6eb867bf06da725ffa64b437ed16792
IrinaPerezhegina <perezeginaiv@mail.ru>
2025-04-28 13:35:53 +0500

    added RootLayout

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,3 @@
 export default function DashboardLayout() {
-  
   return <div className="">dashboard</div>;
 }
-
-// 8:07:19
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,5 @@
 import { Header } from "@/components/shared";
 import { Metadata } from "next";
-import { Nunito } from "next/font/google";
-import "../globals.css";
-
-const nunito = Nunito({
-  subsets: ["cyrillic"],
-  variable: "--font-nunito",
-  weight: ["400", "500", "600", "700", "800", "900"],
-});
 
 export const metadata: Metadata = {
   title: "Pizza | Главная",
@@ -21,10 +13,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={nunito.className}>
-        <Header />
-        <main className="min-h-screen">{children}</main>
-      </body>
+      <Header />
+      <main className="min-h-screen">{children}</main>
     </html>
   );
 }
