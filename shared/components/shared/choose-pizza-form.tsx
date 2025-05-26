53ade7cca69d47151ad86961193febca567c45cf
IrinaPerezhegina <perezeginaiv@mail.ru>
2025-05-26 10:47:05 +0500

    added Ingredient

--- shared/components/shared/choose-pizza-form.tsx
+++ shared/components/shared/choose-pizza-form.tsx
@@ -1,4 +1,9 @@
-import { pizzaSizes } from "@/shared/constants/pizza";
+import {
+  PizzaSize,
+  pizzaSizes,
+  PizzaType,
+  pizzaTypes,
+} from "@/shared/constants/pizza";
 import { cn } from "@/shared/lib/utils";
 import * as React from "react";
 import { Button } from "../ui";
@@ -23,17 +28,32 @@ export const ChoosePizzaForm: React.FC<React.PropsWithChildren<Props>> = ({
   items,
   onClickAdd,
 }) => {
+  const [size, setSize] = React.useState<PizzaSize>(20);
+  const [type, setType] = React.useState<PizzaType>(1);
+
   const textDetails = "30 см, традиционное тесто 30";
   const totalPrice = 500;
-  const size = 30;
+
   return (
     <div className={cn(className, "flex flex-1")}>
       <PizzaImage imageUrl={imageUrl} size={size} />
       <div className="w-[490px] bg-[#f7f6f5] p-7">
         <Title text={name} size="md" className="font-extrabold mb-1" />
         <p className="text-gray-400">{textDetails}</p>
-        <GroupVariants items={pizzaSizes} selectedValue="" />
 
+        <div className="flex flex-col gap-4 mt-5">
+          <GroupVariants
+            items={pizzaSizes}
+            value={String(size)}
+            onClick={(value) => setSize(Number(value) as PizzaSize)}
+          />
+
+          <GroupVariants
+            items={pizzaTypes}
+            value={String(type)}
+            onClick={(value) => setType(Number(value) as PizzaType)}
+          />
+        </div>
         <Button
           // loading={loading}
           // onClick={}
--- shared/components/shared/modals/choose-product-modal.tsx
+++ shared/components/shared/modals/choose-product-modal.tsx
@@ -20,7 +20,7 @@ export const ChooseProductModal: React.FC<Props> = ({ className, product }) => {
     <Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
       <DialogContent
         className={cn(
-          "p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden",
+          "p-0 w-[1060px] max-w-[1060px] min-h-[550px] bg-white overflow-hidden",
           className
         )}
       >
