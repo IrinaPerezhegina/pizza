2c365f54743c8802b2d60e835e9f03d7f1c6a5c2
IrinaPerezhegina <perezeginaiv@mail.ru>
2025-06-11 08:54:27 +0500

    added useForm

--- shared/components/shared/form/form-input.tsx
+++ shared/components/shared/form/form-input.tsx
@@ -27,6 +27,9 @@ export const FormInput: React.FC<Props> = ({
     watch,
     setValue,
   } = useFormContext();
+
+  const value = watch(name);
+  const errorText = errors[name]?.message as string;
   return (
     <div className={className}>
       {label && (
@@ -35,11 +38,11 @@ export const FormInput: React.FC<Props> = ({
         </p>
       )}
       <div className="relative">
-        <Input className="h-12 text-md" {...props} />
-        <ClearButton />
+        <Input className="h-12 text-md" {...props} {...register(name)} />
+        {value && <ClearButton />}
       </div>
 
-      <ErrorText text="Поле обязательно для заполнения" className="mt-2" />
+      {errorText && <ErrorText text={errorText} className="mt-2" />}
     </div>
   );
 };
